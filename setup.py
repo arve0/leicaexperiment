179c22e67f7794d2a62bb99aaa6dc8df42a35e61
arve0 <arve.seljebu@gmail.com>
2015-03-10 16:08:23 +0100

    requirements and setup

--- setup.py
+++ setup.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python
-# encoding: utf-8
 
-# readme
 import os
-if os.path.exists('README.rst'):
-    long_description = open('README.rst').read()
-else:
-    long_description = ''
+import sys
 
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(name='LeicaExperiment',
-      version='0.1.0',
-      description='This project have been renamed to MatrixScreener',
-      author='Arve Seljebu',
-      author_email='arve.seljebu@gmail.com',
-      license='MIT',
-      url='https://github.com/arve0/leicaexperiment',
-      py_modules=['leicaexperiment'],
-      install_requires=['tifffile', 'numpy'],
-      long_description=long_description)
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+os.system('make rst')
+readme = open('README.rst').read()
+
+setup(
+    name='leicaexperiment',
+    version='0.0.1',
+    description='Read, stitch and compress Leica LAS MatrixS Screener experiments',
+    long_description=readme,
+    author='Arve Seljebu',
+    author_email='arve.seljebu@gmail.com',
+    url='https://github.com/arve0/leicaexperiment',
+    packages=[
+        'leicaexperiment',
+    ],
+    package_dir={'leicaexperiment': 'leicaexperiment'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    license='MIT',
+    zip_safe=False,
+    keywords='leicaexperiment',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
