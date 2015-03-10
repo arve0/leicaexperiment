4e3e182516587f7bf1017b7f22aabfb6d2d6ddea
arve0 <arve.seljebu@gmail.com>
2015-03-10 20:11:28 +0100

    install requires from requirements.txt

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@
 
 import os
 import sys
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt')
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
 
 try:
     from setuptools import setup
@@ -29,8 +37,7 @@ setup(
     ],
     package_dir={'leicaexperiment': 'leicaexperiment'},
     include_package_data=True,
-    install_requires=[
-    ],
+    install_requires=reqs,
     license='MIT',
     zip_safe=False,
     keywords='leicaexperiment',
